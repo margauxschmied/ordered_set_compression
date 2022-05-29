3e4a097c56855b379936c97eae873ff9c37e8ca0
margauxschmied <margaux.schmied@icloud.com>
2022-05-29 18:35:57 +0200

    Stream VByte + composition

--- random_dataset.py
+++ random_dataset.py
@@ -4,12 +4,28 @@ import json
 
 from bs4 import SoupStrainer
 
-# n = 1000
-# numMax = 100
+n = 1000000
+numMax = 10000000
 
-# l = []
-# for i in range(n):
-#     l.append(random.randint(0, numMax))
+l = []
+i = 0
+while len(l) < n:
+    a = random.randint(0, numMax)
+    if len(l) != 0:
+        if a not in l:
+            l.append(a)
+    else:
+        l.append(a)
+
+l = sorted(l)
+
+print(l)
+
+l = [str(x) for x in l]
+
+l = " ".join(l)
+
+print(l)
 
 # l = sorted(l)
 # jsonString = json.dumps(l)
@@ -34,26 +50,26 @@ from bs4 import SoupStrainer
 #     file.write(l)
 
 
-with open('dataset/Regin_format.txt', 'r') as fichier:
-    contenu = fichier.read()
+# with open('dataset/Regin_format.txt', 'r') as fichier:
+#     contenu = fichier.read()
 
-contenu = contenu.split("\n")
-l = []
-for c in contenu:
-    c = c.replace(" 0 0", " 0")
-    print(c in " 0 0")
-    # tmp = c.split(" ")
-    # for i in range(0, len(tmp)):
-    #     if tmp[i] != '' and int(tmp[i]) > 1000000000:
-    #         tmp[i] = ''
+# contenu = contenu.split("\n")
+# l = []
+# for c in contenu:
+#     c = c.replace(" 0 0", " 0")
+#     print(c in " 0 0")
+# tmp = c.split(" ")
+# for i in range(0, len(tmp)):
+#     if tmp[i] != '' and int(tmp[i]) > 1000000000:
+#         tmp[i] = ''
 
-    # tmp = [i for i in tmp if i != '']
-    # l.append(" ".join(tmp))
+# tmp = [i for i in tmp if i != '']
+# l.append(" ".join(tmp))
 
-l = "\n".join(contenu)
+# l = "\n".join(contenu)
 
 
-with open('dataset/Regin_format.txt', 'w') as file:
+with open('dataset/artificiel/dataset_' + str(n) + '_' + str(numMax) + '.txt', 'w') as file:
     # file.write(jsonString)
     file.write(l)
 
